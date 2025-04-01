2a397021bb78cc92f0a36ca991a117aca2f3c25a
Bradley S <bradleysantiago4@gmail.com>
2025-03-31 22:15:47 -0400

    initial deploy: correcting certain files from our template

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,2 +1,20 @@
-import db from '../config/connection.js';
+import mongoose from 'mongoose';
+
+// Function to clean database collections
+const cleanDB = async (modelName: string, collectionName: string) => {
+  try {
+    // This will clean/drop the specified collection
+    await mongoose.connection.db.dropCollection(collectionName);
+    console.log(`${modelName} collection cleaned!`);
+  } catch (err) {
+    // If collection doesn't exist, just return
+    if ((err as any).code === 26) {
+      console.log(`${modelName} collection doesn't exist, nothing to clean`);
+      return;
+    }
+    throw err;
+  }
+};
+
+export default cleanDB;
 
--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,4 +1,28 @@
 import db from "../config/connection.js";
 import cleanDB from "./cleanDb.js";
 
+// Simple function to demonstrate usage of imports
+const seedDatabase = async () => {
+  try {
+    // Example of using the db connection
+    const dbStatus = db.readyState === 1 ? 'connected' : 'disconnected';
+    console.log(`Database status: ${dbStatus}`);
+    
+    // Example of using cleanDB 
+    // Uncomment and modify when needed:
+    // await cleanDB('Question', 'questions');
+    
+    console.log('Database seeding complete!');
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+// Uncomment to run seed function:
+// seedDatabase();
+
+export default seedDatabase;
+
 
