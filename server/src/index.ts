8a196f88934ce2bedb89c77e032548288fc0565e
Bradley S <bradleysantiago4@gmail.com>
2025-04-02 20:33:02 -0400

    corrected apollo server support

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas';
+import { typeDefs, resolvers } from './schemas/index.js';
 import { authMiddleware, UserContext } from './utils/auth';
 import db from './config/connection';
 
