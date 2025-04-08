e2872a0a3aa4f22bde941d05846e52edd2564f44
Daniel Gonzalez <dannygonzr20@gmail.com>
2025-04-07 21:43:41 -0400

    Added Watchlist.tsx to show created watchlist page, additionally created functionality within this component to allow users to search movies and add/delete to their watchlist using omdb API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,4 @@
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-=======
-
->>>>>>> 1a4d91e14f14e26d88589ac927f73860a5b5d783
 import App from './App.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
