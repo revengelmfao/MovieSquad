c1e469a41db2fe40053baf0b8f00a4fe398207ce
Bradley S <bradleysantiago4@gmail.com>
2025-04-10 11:04:08 -0400

    stable commit for further testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Profile from "./components/Profile";
 import SignIn from "./components/SignIn";
 import Navbar from "./components/Navbar";
 import WatchlistPage from "./components/Watchlist"; 
+import MovieReviewPage from "./components/MovieReviewPage";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -18,6 +19,7 @@ const App: React.FC = () => {
             <Route path="/profile" element={<Profile />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/watchlist/:watchlistName" element={<WatchlistPage />} />
+            <Route path="/movie/:id" element={<MovieReviewPage />} />
           </Routes>
         </div>
       </AuthProvider>
