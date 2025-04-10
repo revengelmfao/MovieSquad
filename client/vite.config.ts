8bdb6036f23234566e865d92b66c797de05160c0
Bradley S <bradleysantiago4@gmail.com>
2025-04-10 09:24:36 -0400

    feat:reintroduced tailwindv4 - stable MongoAtlasDB, JWT, and ApolloDashboard

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss()
   ],
   build: {
     // Configure alternative bundler settings if needed
